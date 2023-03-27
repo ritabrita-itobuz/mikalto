58119e9a6adeacd60f860b3565b6d74ca8764e48
Tuhin Mukherjee <tuhin@itobuz.com>
2023-03-27 18:53:07 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function display() {
+    // document.getElementById("icon-image").classList.toggle("show-menu");
+    alert("hbhbhb");
+    // document.getElementsByClassName('mikalto').style.display="none";
+    // gsap.fromTo("#menu-display", {x:-500}, {x:0});
+    // alert("hbhbhb");
+    // gsap.fromTo("#display", {x:-500}, {x:0});
+    // alert("jbjhbhjb");
+}
