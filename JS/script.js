a4370622f54c65cbaa457e0dc9b41744bda9c96f
ritabrita_itobuz <ritabrita@itobuz.com>
2023-03-29 14:33:05 +0530

    extra css lines removed

--- JS/script.js
+++ JS/script.js
@@ -4,7 +4,7 @@ let data = async function (){
       await fetch("http://localhost:3000").then((data)=>{
         return data.json()
       }).then((data)=>{
-      document.getElementById("vacation").innerHTML= data.heading;
+    //   document.getElementById("vacation").innerHTML= data.heading;
       document.getElementById("vacation").src= data.image
     }
       );
