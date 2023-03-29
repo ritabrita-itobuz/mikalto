0426d094bb0335a72535b58061923a07a1bf5213
ritabrita_itobuz <ritabrita@itobuz.com>
2023-03-29 18:52:36 +0530

    almost done

--- JS/script.js
+++ JS/script.js
@@ -1,13 +1,23 @@
 // const api="http://localhost:3000";
 // var information;
-let data = async function (){
+const data = async function (){
       await fetch("http://localhost:3000").then((data)=>{
         return data.json()
-      }).then((data)=>{
-    //   document.getElementById("vacation").innerHTML= data.heading;
-      document.getElementById("vacation").src= data.image
+      }).then((data) => {
+      const enjoy = document.getElementById("enjoy-text");
+      const vacation = document.getElementById("vacation-text");
+      const background = document.getElementById("vacation");
+      const hideaway = document.getElementById("beautiful-hideaway");
+      // const rate = document.getElementById("rate-per-night");
+
+      enjoy.innerHTML= data.enjoy;
+      vacation.innerHTML= data.vacation;
+      background.style.backgroundImage= `url(${data.image})`;
+      hideaway.innerHTML= data.hideaway;
+      // rate.innerHTML= data.hideaway;
+      
     }
-      );
+);
        
 }
 
