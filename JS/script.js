1b8529f92fbb40d2f5c57ece2a598a17970cc31f
ritabrita_itobuz <ritabrita@itobuz.com>
2023-03-30 12:54:38 +0530

    bootstrap link is making page overflow

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,4 @@
-// const api="http://localhost:3000";
-// var information;
+
 const data = async function (){
       await fetch("http://localhost:3000").then((data)=>{
         return data.json()
@@ -8,12 +7,22 @@ const data = async function (){
       const vacation = document.getElementById("vacation-text");
       const background = document.getElementById("vacation");
       const hideaway = document.getElementById("beautiful-hideaway");
+      const wellness = document.getElementById("wellness");
+      const wellnesskey = document.getElementById("wellness-subheading");
+      const firstroom = document.getElementById("first-room");
+      const secondroom = document.getElementById("second-room");
+      const thirdroom = document.getElementById("third-room");
       // const rate = document.getElementById("rate-per-night");
 
       enjoy.innerHTML= data.enjoy;
       vacation.innerHTML= data.vacation;
       background.style.backgroundImage= `url(${data.image})`;
       hideaway.innerHTML= data.hideaway;
+      wellness.innerHTML= data.wellness;
+      wellnesskey.innerHTML= data.wellnesskey;
+      firstroom.src= data.singleRoomPicture;
+      secondroom.src= data.doubleRoomPicture;
+      thirdroom.src= data.thirdRoomPicture;
       // rate.innerHTML= data.hideaway;
       
     }
